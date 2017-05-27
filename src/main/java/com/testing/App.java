61fb8cce28659dc71816ee52611d0ab3f691f22a
Paul Yorde <paulyorde26@gmail.com>
2017-05-27 13:10:16 -0400

    Initial commit

---
+++ src/main/java/com/testing/App.java
@@ -0,0 +1,33 @@
+package com.testing;
+
+
+import org.apache.tika.Tika;
+
+import javax.activation.FileTypeMap;
+import java.nio.file.*;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+
+public class App
+{
+    public static final String FILE_TYPE = "text/csv";
+    public static final String DIR = "/Users/origin/tmp";
+
+    public static void main( String[] args ) throws Exception {
+        Path dir = Paths.get(DIR);
+        final Tika tika = new Tika();
+        WatchService watchService = FileSystems.getDefault().newWatchService();
+        dir.register(watchService, ENTRY_CREATE);
+
+        WatchKey key;
+        do {
+             key = watchService.take();
+             key.pollEvents().stream().filter(e -> {
+             Path fileName = (Path)e.context();
+             String type = tika.detect(fileName.toString());
+                 return FILE_TYPE.equals(type);
+             }).forEach(e -> System.out.printf("File found %s%n",e.context()));
+        } while (key.reset());
+    }
+
+}
